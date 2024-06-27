58c2b73adb09a08dd345549f5a3cb9ec65847faa
ROUGGA <rouggama@gmail.com>
2024-06-27 03:00:20 +0200

    Server crash bug fix
    user can chose what window to login to from the login page.

--- web/js/login.js
+++ web/js/login.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $("#err").hide();
+    $("#window").val($("#selectWindowSelect").val());
+    $("#windowText").val($("#selectWindowSelect option:selected").text());
     $("#errClose").on('click', function () {
         $("#err").hide();
     });
@@ -12,30 +14,35 @@ $(document).ready(function () {
 
 
 
-    $("#submit").on('click', function () {
-        var url = getPath().base + "client/login";
-        var par = {};
-        par.username = $("#username").val();
-        par.password = $("#password").val();
-        par.branchId = $("#branchId").val();
-        par.windowText = $("#windowText").val();
-        par.rand = Math.random();
-        par.window = $("#window").val();
-        par.host = localStorage.getItem("ipSS");
-        par.port = localStorage.getItem("portSS");
+    $("#submit2").on('click', function () {
+        if (getPath()) {
+            var url = getPath().base + "client/login";
+            var par = {};
+            par.username = $("#username").val();
+            par.password = $("#password").val();
+            par.branchId = $("#branchId").val();
+            par.windowText = $("#windowText").val();
+            par.rand = Math.random();
+            par.window = $("#window").val();
+            par.host = localStorage.getItem("ipSS");
+            par.port = localStorage.getItem("portSS");
+            console.log(par);
+            $.post(url, par, function (data) {
+                if (data.indexOf("login_center") >= 0) {
+                    $("#err").show();
+                    $("#errText").html("L'identifiant ou le mot de passe est incorrect");
+                    console.log("Connection to pdjh BACKEND IS FAILED!!");
+                } else {
+                    console.log("Connected to pdjh BACKEND, redirecting to home page...");
+                    par.status = 1;
+                    $.post("./ChangeStatus", par, function (data) {
+                        window.location = "./home.jsp";
+                    });
+                }
+            });
+        } else {
+            $('#settingsModal').modal('toggle');
+        }
 
-        $.post(url, par, function (data) {
-            if (data.indexOf("login_center") >= 0) {
-                $("#err").show();
-                $("#errText").html("L'identifiant ou le mot de passe est incorrect");
-                console.log("Connection to pdjh BACKEND IS FAILED!!");
-            } else {
-                console.log("Connected to pdjh BACKEND, redirecting to home page...");
-                par.status = 1;
-                $.post("./ChangeStatus", par, function (data) {
-                    window.location = "./home.jsp";
-                });
-            }
-        });
     });
 });
